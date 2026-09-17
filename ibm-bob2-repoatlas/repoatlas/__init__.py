51f69ec05ac6188772b40f7ece1448ab1cdd51e3
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-17 01:42:37 -0400

    Start IBM Bob 2.0 RepoAtlas carrier

---
+++ ibm-bob2-repoatlas/repoatlas/__init__.py
@@ -0,0 +1,5 @@
+"""RepoAtlas deterministic repository-onboarding and architecture-drift evidence."""
+
+from .core import RepoAtlasError, compile_packet, verify_bundle
+
+__all__ = ["RepoAtlasError", "compile_packet", "verify_bundle"]
